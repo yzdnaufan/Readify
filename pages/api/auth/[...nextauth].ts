7ec2fab13ac8ce857f28064dd5802ab284110673
yzdnaufan <ahyaznau@gmail.com>
2023-05-08 22:43:38 +0700

    feat : fix 0auth token and session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,49 @@
 import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
+    // adapter: PrismaAdapter(prisma),
+    session: {
+      strategy:"jwt",
+
+      maxAge: 60*60, //1 Hrs
+    },
+    callbacks: {
+      async session({token, session}){
+        if (token){
+          session.user = token.user
+        }
+        return session
+      },
+      async jwt({token, user, account, profile, isNewUser}){
+
+        if (account?.provider === "google" || account?.provider === "github"){
+          token.user = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          }
+        }
+        return token
+      }
+    },
+
     providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
       }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        
+      })
     ], 
   }
 
---
+++ pages/api/v1/restricted.ts
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt"
+import { getSession } from "next-auth/react"
+import { authOptions } from "../auth/[...nextauth]"
+
+export default async (req, res) => {
+  const session = await getSession({req})
+
+  
+  if (session) {
+    // Signed in
+    res.status(200).json({ name: session.user.name, email: session.user.email })
+  } else {
+    // Not Signed in
+    res.status(401)
+  }
+  res.end()
+}
